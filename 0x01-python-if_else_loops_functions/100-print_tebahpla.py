db7af9bbbd5f108ea21e635df1a27cf8681d141f
niyioo <niyiolagbegi@gmail.com>
2023-06-06 15:03:55 -0700

    fix 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print(chr(i).lower() if i % 2 == 1 else chr(i).upper(), end='')
+    print("{}".format(chr(i).lower() if i % 2 == 1 else chr(i).upper()), end='')
