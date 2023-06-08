c7a8d5bfb832ace3121bcf0852a5496ce8d14a8c
niyioo <niyiolagbegi@gmail.com>
2023-06-08 07:42:14 -0700

    4-hidden_discovery.py fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,9 @@
 import hidden_4
 
 if __name__ == "__main__":
-    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    module_names = [
+            name for name in dir(hidden_4) if not name.startswith("__")
+            ]
     module_names.sort()
 
     for name in module_names:
