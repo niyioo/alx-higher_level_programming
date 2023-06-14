df686529a4163749bc3fef47b3e082b744d0661a
niyioo <niyiolagbegi@gmail.com>
2023-06-14 13:58:43 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,80 +1,40 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
 
 /**
- * print_python_bytes - Prints information about a Python bytes object
- * @p: Pointer to the Python bytes object
- *
- */
-void print_python_bytes(PyObject *p)
-{
-	PyBytesObject *bytes = (PyBytesObject *)p;
-	Py_ssize_t size, i;
-
-	printf("[.] bytes object info\n");
-
-	size = PyBytes_Size(p);
-	printf("  size: %ld\n", size);
-
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-
-	printf("  first 10 bytes: ");
-	for (i = 0; i < size && i < 10; i++)
-		printf("%02x ", bytes->ob_sval[i] & 0xff);
-	printf("\n");
-}
-
-/**
- * print_python_list - Prints information about a Python list object
- * @p: Pointer to the Python list object
- *
+ * print_python_list - Prints basic information about a Python list
+ * @p: Pointer to the PyObject representing the Python list
  */
 void print_python_list(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
-	Py_ssize_t size, i;
-	PyObject *element;
-
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t allocated = list->allocated;
 	printf("[*] Python list info\n");
-
-	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %ld\n", size);
-
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
+	printf("[*] Allocated = %ld\n", allocated);
+	for (Py_ssize_t i = 0; i < size; i++)
 	{
-		element = PyList_GetItem(p, i);
-		printf("Element %ld: ", i);
-
-		if (PyBytes_Check(element))
-		{
-			printf("bytes\n");
-			print_python_bytes(element);
-		}
-		else if (PyFloat_Check(element))
-		{
-			printf("float\n");
-		}
-		else if (PyTuple_Check(element))
-		{
-			printf("tuple\n");
-		}
-		else if (PyList_Check(element))
-		{
-			printf("list\n");
-		}
-		else if (PyLong_Check(element))
-		{
-			printf("int\n");
-		}
-		else if (PyUnicode_Check(element))
-		{
-			printf("str\n");
-		}
-		else
-		{
-			printf("Unknown type\n");
-		}
+		PyObject *item = PyList_GetItem(p, i);
+		const char *type = Py_TYPE(item)->tp_name;
+		printf("Element %ld: %s\n", i, type);
 	}
+}
+
+/**
+ * print_python_bytes - Prints basic information about a Python bytes object
+ * @p: Pointer to the PyObject representing the Python bytes object
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	Py_ssize_t size = PyBytes_GET_SIZE(p);
+	const char *string = PyBytes_AS_STRING(p);
+	printf("[.] bytes object info\n");
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+	printf("  first %ld bytes: ", (size < 10) ? size + 1 : 10);
+	for (Py_ssize_t i = 0; i < size && i < 10; i++)
+		printf("%02x ", (unsigned char)string[i]);
+	printf("\n");
 }
