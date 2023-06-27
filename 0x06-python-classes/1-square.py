6a85768fc413e2b15c4fc4de9d53295110e05165
niyioo <niyiolagbegi@gmail.com>
2023-06-27 04:57:46 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a square shape.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
