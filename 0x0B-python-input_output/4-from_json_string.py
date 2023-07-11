cff762d86b1befd47e16152cbda658d7a96db71d
niyioo <niyiolagbegi@gmail.com>
2023-07-11 03:50:12 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that returns an object """
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Return the Python data structure represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to convert to a Python object.
+
+    Returns:
+        Any: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
