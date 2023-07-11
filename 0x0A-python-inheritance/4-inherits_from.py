f0f7d116d307d9d6cf2eb9e9e342e30b2503b17c
niyioo <niyiolagbegi@gmail.com>
2023-07-10 19:23:08 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module for same class or inherit from  """
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if an object is an instance of a class
+    that inherited (directly or indirectly) from
+    the specified class.
+
+    Args:
+        obj: An object.
+        a_class: A class to compare against.
+
+    Returns:
+        True if the object is an instance of a class
+        that inherited (directly or indirectly) from
+        the specified class; False otherwise.
+
+    """
+    return issubclass(type(obj), a_class)
