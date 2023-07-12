c0db7efa29ac3230f44a7d0b47357cc94ad57370
niyioo <niyiolagbegi@gmail.com>
2023-07-11 17:20:41 -0700

    fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,43 +5,36 @@
 import sys
 
 
-def print_info():
-    print('File size: {:d}'.format(file_size))
-
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
-
 status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 total_file_size = 0
 status_counts = {code: 0 for code in status_codes}
 
 line_count = 0
-file_size = 0
-
 try:
     for line in sys.stdin:
-        if line_count != 0 and line_count % 10 == 0:
-            print_info()
-
-        pieces = line.split()
-
-        try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
-            pass
-
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
-
         line_count += 1
 
-    print_info()
+        """Extract information from the line"""
+        line_parts = line.split(' ')
+        if len(line_parts) >= 7:
+            status_code = line_parts[-2]
+            file_size = int(line_parts[-1])
+            total_file_size += file_size
+
+            """Count status codes"""
+            if status_code in status_counts:
+                status_counts[status_code] += 1
+
+        """Print statistics every 10 lines"""
+        if line_count % 10 == 0:
+            print("File size: {:d}".format(total_file_size))
+            for code in sorted(status_counts.keys()):
+                if status_counts[code] > 0:
+                    print("{:s}: {:d}".format(code, status_counts[code]))
+
 except KeyboardInterrupt:
-    print_info()
-    raise
+    """Print final statistics on keyboard interruption"""
+    print("File size: {:d}".format(total_file_size))
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print("{:s}: {:d}".format(code, status_counts[code]))
