aafb1fb9e1c2f81ab74f810163c63b678b9e4e71
niyioo <niyiolagbegi@gmail.com>
2023-07-11 17:03:37 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Moduke for log parsing """
+""" Module for log parsing """
 
 
 import sys
@@ -9,23 +9,23 @@ status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 total_file_size = 0
 status_counts = {code: 0 for code in status_codes}
 
+line_count = 0
 try:
-    line_count = 0
     for line in sys.stdin:
         line_count += 1
 
-        """Extract information from the line"""
+        # Extract information from the line
         line_parts = line.split(' ')
         if len(line_parts) >= 7:
             status_code = line_parts[-2]
             file_size = int(line_parts[-1])
             total_file_size += file_size
 
-            """Count status codes"""
+            # Count status codes
             if status_code in status_counts:
                 status_counts[status_code] += 1
 
-        """Print statistics every 10 lines"""
+        # Print statistics every 10 lines
         if line_count % 10 == 0:
             print("File size: {:d}".format(total_file_size))
             for code in sorted(status_counts.keys()):
@@ -33,7 +33,7 @@ try:
                     print("{:s}: {:d}".format(code, status_counts[code]))
 
 except KeyboardInterrupt:
-    """Print final statistics on keyboard interruption"""
+    # Print final statistics on keyboard interruption
     print("File size: {:d}".format(total_file_size))
     for code in sorted(status_counts.keys()):
         if status_counts[code] > 0:
