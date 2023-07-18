9c49110c8948b554fc26ea83969d61bb332fcb9b
niyioo <niyiolagbegi@gmail.com>
2023-07-18 02:34:27 -0700

    update rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -172,7 +172,8 @@ class Rectangle(Base):
         Returns:
             str: Rectangle representation.
         """
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
+                {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
         """
