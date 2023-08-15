5135d86c26c6a91ca98e103f2daeb37af670f5fb
niyioo <niyiolagbegi@gmail.com>
2023-08-15 12:56:58 +0100

    no cheating

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Task: Update the score of the record with name "Bob" in the table second_table.
+
+-- Update the score of the record with name "Bob".
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
