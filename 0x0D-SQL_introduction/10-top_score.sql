bce9491352afbb78f2814c53e9f3ed2122f91c99
niyioo <niyiolagbegi@gmail.com>
2023-08-15 12:52:14 +0100

    top score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,9 +1,6 @@
 -- Task: List all records of the table second_table from the specified database.
 
--- Set the database name provided as an argument.
-SET @db_name = hbtn_0c_0;
-
 -- Show all records from the table, ordered by score (top first).
 SELECT score, name
-FROM @hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
