18b5c681a4aabef3ea4ee8b043998fae6ae3c54a
niyioo <niyiolagbegi@gmail.com>
2023-08-16 14:58:17 +0100

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Show grants for the user user_0d_1 and user user_0d_2 on the localhost host
-
+-- Script that shows grants for the user user_0d_1 and user user_0d_2 on the localhost host
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
