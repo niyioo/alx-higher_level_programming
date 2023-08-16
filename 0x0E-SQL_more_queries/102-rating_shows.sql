61f841e28fc21020d943ff8332f8f170309ead75
niyioo <niyiolagbegi@gmail.com>
2023-08-16 19:22:46 +0100

    fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List shows and their rating sum, sorted by descending rating
-SELECT tv_shows.title, SUM(tv_shows_rate.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
-INNER JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show_id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_show_ratings.show_id
 ORDER BY rating_sum DESC;
