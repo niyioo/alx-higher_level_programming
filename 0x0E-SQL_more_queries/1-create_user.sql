9be14eb58413787c471b00c12b5de6ae5059bd82
niyioo <niyiolagbegi@gmail.com>
2023-08-16 15:06:40 +0100

    second

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- script that creates the MySQL server user user_0d_1 and grant all privileges to the user
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
