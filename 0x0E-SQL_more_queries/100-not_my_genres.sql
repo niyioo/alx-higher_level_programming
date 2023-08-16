5f3193b8f74ad61d935401d2127ea8c8526dfe5b
niyioo <niyiolagbegi@gmail.com>
2023-08-16 16:49:50 +0100

    not my genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- Get all genres
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
