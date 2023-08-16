756c578bbaa0f8cf9131a4a838e339eab1a26ab5
niyioo <niyiolagbegi@gmail.com>
2023-08-16 19:24:26 +0100

    fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- List genres and their rating sum, sorted by descending rating
-SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
