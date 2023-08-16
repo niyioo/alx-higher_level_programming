a0ca3b04295929d50997ee90e292710560f2f91d
niyioo <niyiolagbegi@gmail.com>
2023-08-16 19:20:03 +0100

    nnnnn

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List shows and their rating sum, sorted by descending rating
 SELECT tv_shows.title, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_shows
-JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_shows.id
+INNER JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show_id
+GROUP BY tv_show_ratings.show_id
 ORDER BY rating_sum DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,7 @@
 -- List genres and their rating sum, sorted by descending rating
 SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.show_id
-GROUP BY tv_genres.id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY rating_sum DESC;
