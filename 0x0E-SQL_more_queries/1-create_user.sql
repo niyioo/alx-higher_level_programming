069affb9238d2fd2912432bec03d007cd6d14bbf
niyioo <niyiolagbegi@gmail.com>
2023-08-17 10:41:19 +0100

    ddd

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,12 +1,8 @@
 -- Create the user 'user_0d_1' with the password 'user_0d_1_pwd' if the user doesn't already exist
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'
-    IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases and tables to the user 'user_0d_1' when connecting from the 'localhost' host
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- flush to reload
 FLUSH PRIVILEGES;
