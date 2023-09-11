5263290fe59be0906e796dd3115e8b7a2fcd97fa
niyioo <niyiolagbegi@gmail.com>
2023-09-11 04:55:36 -0700

    fix 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,8 +3,4 @@
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if (arg1 === undefined || arg2 === undefined) {
-  console.log('Usage: node script.js <arg1> <arg2>');
-} else {
-  console.log(arg1 + ' is ' + arg2);
-}
+console.log(arg1 + ' is ' + arg2);
