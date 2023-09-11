a2ac8d8756eba11ad9f653e89eae997dac61ae93
niyioo <niyiolagbegi@gmail.com>
2023-09-11 07:41:13 -0700

    fix

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12,
-    incr: function () {
-        this.value++;
-    }
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+ 
+myObject.incr: function () {
+    this.value++;
+  }
 };
 console.log(myObject);
 myObject.incr();
