f8b79718da3ed2cafad4f84fcf7f7ab9fe74b973
niyioo <niyiolagbegi@gmail.com>
2023-09-11 05:09:17 -0700

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const x = parseInt(process.argv[2]);
 
 if (!isNaN(x)) {
-	for (let i = 0; i < x; i++) {
-		console.log("C is fun");
-	}
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 } else {
-	console.log("Missing number of occurrences");
+  console.log("Missing number of occurrences");
 }
