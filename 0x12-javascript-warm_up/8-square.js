702e5b4c7fd015ab4c5066a0e8b13e197941cf9b
niyioo <niyiolagbegi@gmail.com>
2023-09-11 05:23:12 -0700

    8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
-
 const size = parseInt(process.argv[2]);
-
 if (!isNaN(size)) {
-	if (size > 0) {
-		for (let i = 0; i < size; i++) {
-			console.log('X'.repeat(size));
-		}
-	} else {
-		console.log('');
-	}
+  if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 } else {
-	console.log('Missing size');
+    console.log('');
+  }
+} else {
+    console.log('Missing size');
 }
