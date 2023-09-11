a79f0bc38e170208728efaf9e3f60cc759053dde
niyioo <niyiolagbegi@gmail.com>
2023-09-11 13:47:42 +0100

    10 11

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial(n) {
+    if (isNaN(n)) {
+        return 1; // Factorial of NaN is 1
+    }
+    if (n === 0 || n === 1) {
+        return 1; // Factorial of 0 and 1 is 1
+    } else {
+        return n * factorial(n - 1); // Recursive calculation of factorial
+    }
+}
+
+const arg = parseInt(process.argv[2]);
+
+const result = factorial(arg);
+console.log(result);
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+function findSecondLargest(arr) {
+    if (arr.length < 2) {
+        return 0;
+    }
+
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let num of arr) {
+        num = parseInt(num);
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
+    }
+
+    return secondMax;
+}
+
+const args = process.argv.slice(2);
+
+const secondLargest = findSecondLargest(args);
+console.log(secondLargest);
