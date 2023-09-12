5ec2cfbee9bad5d216cad877e900af2761102a20
niyioo <niyiolagbegi@gmail.com>
2023-09-13 00:48:06 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 // Import the list from 100-data.js
-const { list } = require('./100-data');
+const list = require('./100-data').list;
 
 // Use the map function to create a new array
 const newList = list.map((value, index) => value * index);
 
 // Print both the initial list and the new list
-console.log('Initial List:', list);
-console.log('New List:', newList);
+console.log(list);
+console.log(newList);
