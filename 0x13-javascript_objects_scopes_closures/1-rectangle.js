a908766df2762562934e0e95f34f689a75cfc05f
niyioo <niyiolagbegi@gmail.com>
2023-09-12 23:33:28 +0100

    ff

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-    // Check if both w and h are positive numbers
-      if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
