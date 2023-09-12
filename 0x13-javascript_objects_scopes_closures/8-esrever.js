19e06c68bc47c1fe0840ae68eeb4a71400023d56
niyioo <niyiolagbegi@gmail.com>
2023-09-12 16:23:39 -0700

    fix 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,19 +2,18 @@
 
 // Function to reverse a list without using the built-in reverse method
 exports.esrever = function (list) {
-    // Check if the list is empty or has only one element (already reversed)
-    if (list.length <= 1) {
-      return list.slice(); // Return a copy of the original list
-    }
-  
-    // Initialize an empty result array
-    const reversedList = [];
-  
-    // Iterate through the original list in reverse order and append elements to the result array
-    for (let i = list.length - 1; i >= 0; i--) {
-      reversedList.push(list[i]);
-    }
-  
-    return reversedList;
-  };
-  
+  // Check if the list is empty or has only one element (already reversed)
+  if (list.length <= 1) {
+    return list.slice(); // Return a copy of the original list
+  }
+
+  // Initialize an empty result array
+  const reversedList = [];
+
+  // Iterate through the original list in reverse order and append elements to the result array
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+
+  return reversedList;
+};
