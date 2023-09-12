bd230e6503724d24db96b2b4f1fc738d37190daf
niyioo <niyiolagbegi@gmail.com>
2023-09-12 23:53:05 +0100

    gg

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object if w or h is not a positive integer
+        return {};
+      }
+    }
+  
+    print() {
+      if (this.width && this.height) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
