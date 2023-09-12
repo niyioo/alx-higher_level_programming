b15695a6987045e3f4fddc65b0403aea29877ce3
niyioo <niyiolagbegi@gmail.com>
2023-09-13 00:16:44 +0100

    7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// Function to count the number of occurrences of a specific element in a list
+exports.nbOccurences = (list, searchElement) => {
+    // Initialize a count variable to keep track of occurrences
+    let count = 0;
+  
+    // Iterate through the list using a forEach loop
+    list.forEach((element) => {
+      if (element === searchElement) {
+        count++;
+      }
+    });
+  
+    // Return the final count
+    return count;
+  };
+  
