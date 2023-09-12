c3a56b797a6383f51b430585a1dd67128f76c917
niyioo <niyiolagbegi@gmail.com>
2023-09-12 16:30:28 -0700

    fix 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,8 +2,7 @@
 
 // Function to convert a number from base 10 to another base
 exports.converter = function (base) {
-    return function (number) {
-      return number.toString(base);
-    };
+  return function (number) {
+    return number.toString(base);
   };
-  
+};
