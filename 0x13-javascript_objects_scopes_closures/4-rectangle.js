0113211d525bd67cf50ae0f4bf03c888e7d96294
niyioo <niyiolagbegi@gmail.com>
2023-09-12 16:03:40 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,41 +1,40 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        // Create an empty object if w or h is not a positive integer
-        return {};
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer
+      return {};
     }
-  
-    print() {
-      if (this.width && this.height) {
-        for (let i = 0; i < this.height; i++) {
-          console.log('X'.repeat(this.width));
-        }
-      }
-    }
-  
-    rotate() {
-      // Exchange the width and height
-      if (this.width && this.height) {
-        const temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-      }
-    }
-  
-    double() {
-      // Double the width and height
-      if (this.width && this.height) {
-        this.width *= 2;
-        this.height *= 2;
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
       }
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  rotate () {
+    // Exchange the width and height
+    if (this.width && this.height) {
+      const temp = this.width;
+      this.width = this.height;
+      this.height = temp;
+    }
+  }
+
+  double () {
+    // Double the width and height
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
