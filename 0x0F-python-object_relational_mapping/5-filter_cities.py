71fcc055d684ff3f40aae0983c27449f4cc7d72b
niyioo <niyiolagbegi@gmail.com>
2023-09-14 05:23:11 -0700

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+A script to list all cities of a specified state from a MySQL database.
+
+Usage: ./5-filter_cities.py <username> <password> <database> <state_name>
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Connects to a MySQL database and lists all cities of the specified state.
+    """
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: ./5-cities_by_state.py <username><password> <database> "
+              "<state_name>")
+        sys.exit(1)
+
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute the SQL query to select cities of the specified state
+        cursor.execute("SELECT GROUP_CONCAT(cities.name "
+                       "ORDER BY cities.id SEPARATOR ', ') "
+                       "FROM cities "
+                       "JOIN states ON cities.state_id = states.id "
+                       "WHERE states.name = %s",
+                       (state_name,))
+
+        # Fetch the concatenated city names
+        cities = cursor.fetchone()[0]
+
+        # Display the concatenated city names
+        print(cities)
+
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
