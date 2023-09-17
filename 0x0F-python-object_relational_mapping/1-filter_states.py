0e1fa8c4172c0fa27d8078ccb0837be7a3110296
niyioo <niyiolagbegi@gmail.com>
2023-09-17 12:29:21 -0700

    fix 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,17 +14,14 @@ def main():
     Connects to a MySQL database and lists states
     with names starting with 'N'.
     """
-    # Check if the correct number of arguments are provided
     if len(sys.argv) != 4:
         print("Usage: ./1-filter_states.py <username> <password> <database>")
         sys.exit(1)
 
-    # Retrieve command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -33,25 +30,19 @@ def main():
         db=database
     )
 
-    # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to select states with names
-    # starting with 'N' and order by states.id
     cursor.execute(
         "SELECT * FROM states "
         "WHERE name LIKE 'N%' "
         "ORDER BY states.id"
     )
 
-    # Fetch all the rows from the result set
     rows = cursor.fetchall()
 
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
     cursor.close()
     db.close()
 
