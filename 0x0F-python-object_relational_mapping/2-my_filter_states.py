e120976c0c0d4ca9358d8369a5a00ff21cfe1ff4
niyioo <niyiolagbegi@gmail.com>
2023-09-17 21:05:15 +0100

    g

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,61 +7,34 @@ Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
 """
 
 import MySQLdb
-import sys
-
-
-def main():
-    """
-    Connects to a MySQL database and retrieves states based on user input.
-    """
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: ./2-my_filter_states.py <username> <password> "
-              "<database> <state_name>")
-        sys.exit(1)
-
-    # Retrieve command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-
-        # Create a cursor object
-        cursor = db.cursor()
-
-        # Execute the SQL query to select states based on user input
-        cursor.execute(
-            "SELECT * FROM states "
-            "WHERE name = %s "
-            "ORDER BY states.id",
-            (state_name,)
-        )
-
-        # Fetch all the rows from the result set
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-        # Close the cursor and the database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-        sys.exit(1)
+from sys import argv
 
 
 if __name__ == "__main__":
-    main()
+    """
+    Connects to a MySQL database and retrieves states based on user input.
+    """
+        # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+    
+    # Execute the SQL query to select states based on user input
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY states.id ASC".format(argv[4]),
+    )
+
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
