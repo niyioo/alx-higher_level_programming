7b5d6ba86a0e298fd3f76ff71ef0d27f97bf5de4
niyioo <niyiolagbegi@gmail.com>
2023-09-28 04:51:01 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | [ "$(cat)" -eq 200 ] && curl -s "$1"
+curl -sL "$1"
