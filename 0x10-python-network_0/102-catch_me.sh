6a65f0f640a093380f224a4050c5e412cfe40ca4
niyioo <niyiolagbegi@gmail.com>
2023-09-28 05:20:43 -0700

    l

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request and displays the response
-curl -s -L -X PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
