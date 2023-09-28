2b47c13d1d3b7d79d45b5354bae480c50e7135ac
niyioo <niyiolagbegi@gmail.com>
2023-09-28 04:43:51 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a request to a URL and
 # displays the size of the response body in bytes
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
