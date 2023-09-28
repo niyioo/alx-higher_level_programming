b7fd9c5e9c003af357fdfd90cb36fd11c839e838
niyioo <niyiolagbegi@gmail.com>
2023-09-28 04:48:11 -0700

    d

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and
-# displays the size of the response body in bytes
+# This script sends a request to a URL and displays the size of the response body in bytes
 curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body of a 200 status code response
-#!/bin/bash
 curl -s -o /dev/null -w "%{http_code}" "$1" | [ "$(cat)" -eq 200 ] && curl -s "$1"
