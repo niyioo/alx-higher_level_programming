ddadadcda62b88c8a2cf00a626dbe9a93365ce97
niyioo <niyiolagbegi@gmail.com>
2023-09-28 05:00:36 -0700

    j

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL and displays the size of the response body in bytes
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a POST request to a URL with custom variables and displays the response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
