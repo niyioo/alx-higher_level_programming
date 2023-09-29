3bd1de5e1fcbbaea2582083e9068a9d0fddb60c9
niyioo <niyiolagbegi@gmail.com>
2023-09-29 12:05:58 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
 """
-This script uses Basic Authentication with a personal
-access token to access GitHub user information
-and displays the user ID.
+This script authenticates with the GitHub API using
+Basic Authentication and retrieves the user's ID.
 """
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 def get_github_user_id(username, access_token):
     """
     Sends a GET request to the GitHub API to retrieve
-    user information and display the user ID.
+    user information and displays the user ID.
 
     Args:
-        username (str): Your GitHub username.
-        access_token (str): Your personal access token
-        (used as a password).
+        username (str): GitHub username.
+        access_token (str): Personal access token used as a password.
 
     Returns:
-        None: Displays the user ID.
+        None: Displays the user's GitHub ID or appropriate error messages.
 
     Raises:
-        requests.exceptions.RequestException: If an
-        error occurs during the request.
+        requests.exceptions.RequestException: If
+        an error occurs during the request.
     """
-    url = f"https://api.github.com/users/{username}"
-    headers = {"Authorization": f"token {access_token}"}
+    auth = HTTPBasicAuth(username, access_token)
+    response = requests.get("https://api.github.com/user", auth=auth)
 
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        
         user_data = response.json()
-        user_id = user_data.get('id')
-        
+        user_id = user_data.get("id")
         if user_id is not None:
-            print(f"GitHub User ID: {user_id}")
+            print(user_id)
         else:
             print("User ID not found in the response.")
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    except ValueError:
+        print("Not a valid JSON response.")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./script_name.py <GitHub Username> "
+              "<Personal Access Token>")
+        sys.exit(1)
+
     username = sys.argv[1]
     access_token = sys.argv[2]
     get_github_user_id(username, access_token)
