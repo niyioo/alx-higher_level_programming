9357c4d021ac9ad84468e20ff7be86aab2283622
niyioo <niyiolagbegi@gmail.com>
2023-10-24 04:54:00 -0700

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const filePath = process.argv[2]; // Get the file path from the first command line argument
-const content = process.argv[3]; // Get the string to write from the second command line argument
-
-if (!filePath || !content) {
-  console.error('Usage: node 1-writeme.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
-// Write the content to the file
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
-  }
-});
+fs.writeFileSync(process.argv[2], process.argv[3]);
