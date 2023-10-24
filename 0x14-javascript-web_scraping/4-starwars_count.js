64c97e1970299206833dee3eea0d1f2d9d845641
niyioo <niyiolagbegi@gmail.com>
2023-10-24 05:13:18 -0700

    fix 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,27 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const apiUrl = process.argv[2]; // Get the API URL from the command line argument
-
-if (!apiUrl) {
-  console.error('Usage: node 4-starwars_count.js <api_url>');
-  process.exit(1);
-}
-
-const characterId = 18; // Character ID for Wedge Antilles
-
-// Make a GET request to the Star Wars API to fetch the list of films
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    const filmData = JSON.parse(body);
-    const wedgeAntillesFilms = filmData.results.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(wedgeAntillesFilms.length);
-  } else {
-    console.error(`Request failed with status code: ${response.statusCode}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const movieData = JSON.parse(body).results;
+    console.log(movieData.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
