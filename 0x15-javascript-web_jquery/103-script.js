bb33b656c9bb06524b81a2f79f1b935982736301
niyioo <niyiolagbegi@gmail.com>
2023-11-01 23:38:37 +0100

    advanced

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#btn_translate').click(function() {
+      translateHello();
+    });
+  
+    $('#language_code').keypress(function(event) {
+      if (event.which === 13) {
+        translateHello();
+      }
+    });
+  
+    function translateHello() {
+      const languageCode = $('#language_code').val();
+      $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+        $('#hello').text(data.hello);
+      });
+    }
+  });  
